cd4194bd8049bc00af2b84adc883ccde3a77a83d
Renato Adorno <renatowillian.2000@gmail.com>
2023-01-19 06:42:08 -0300

    ✨ feat: adds content

---
+++ src/components/content/content.stitche.ts
@@ -0,0 +1,29 @@
+import { styled } from '../../../stitches.config';
+
+export const Container = styled('section', {
+  gridColumn: 2,
+  backgroundColor: '$gray6',
+});
+
+
+
+
+
+// variants:{
+//   variant: {
+//     max: {
+//       marginLeft: '18%',
+//       flex: 8,
+//     },
+//     min: {
+//       marginLeft: '5%',
+//       flex: 8,
+//     },
+//     none: {
+//     },
+//   },
+// },
+
+// defaultVariants: {
+//   variant: 'max'
+// },
