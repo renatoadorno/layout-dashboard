534b7dfac54a5a3e0cafee3dad43f2687791af5e
Renato Adorno <renatowillian.2000@gmail.com>
2023-01-19 06:41:37 -0300

    ✨ feat: adds sidebar

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,100 @@
+// import { Container } from './sidebar.stitche';
+import { BsDashSquareDotted } from 'react-icons/bs';
+
+export default function Sidebar() {
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+        <li>
+          <BsDashSquareDotted />
+          <p>Dasboard</p>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/sidebar/sidebar.stitche.ts
@@ -0,0 +1,10 @@
+import { styled } from '../../../stitches.config';
+
+export const Container = styled('nav', {
+  gridColumn: 1,
+  // width: '18%',
+  height: '85vh',
+  backgroundColor: '$gray8',
+  position: 'sticky',
+  left: 0,
+});
